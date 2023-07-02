2af00e8285cc978afdf9c587400dd7b9ffe88cb5
Will Ryan <will@willryan.ca>
2023-07-01 23:07:38 -0700

    Upgrade tests/packages to latest versions

---
+++ cypress.config.ts
@@ -0,0 +1,9 @@
+export default {
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+  },
+}
--- cypress/e2e/examples/navigation.cy.ts
+++ cypress/e2e/examples/navigation.cy.ts
@@ -1,6 +1,6 @@
 context('Navigation', () => {
   it('can visit percy', () => {
     cy.visit('https://percy.io')
-    cy.percySnapshot('home page', {widths: [1024]})
+    cy.percySnapshot('home page', {widths: [1024, 1280, 1920]})
   })
 })
